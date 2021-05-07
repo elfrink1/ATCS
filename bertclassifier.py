dc0bd74c58b848eabe972566bd39425a8efd70e7
lodewijkvkeizerswaard <lodewijk.keizer@gmail.com>
2021-05-07 12:48:57 +0200

    Set pool layer to finetune if finetunelayers > 0

--- bertclassifier.py
+++ bertclassifier.py
@@ -8,10 +8,11 @@ class BertClassifier(nn.Module):
         self.bert = BertModel.from_pretrained("bert-base-uncased")
         self.nr_layers = 11
         self.finetuned_layers = [str(self.nr_layers - diff) for diff in range(conf.finetuned_layers)] if conf.finetuned_layers > 0 else []
-        self.finetuned_layers.append("pooler")
+        if self.finetuned_layers != []:
+            self.finetuned_layers.append("pooler")
 
         for n, p in self.bert.named_parameters():
-            if True in [ftl in n for ftl in self.finetuned_layers]:
+            if self.finetuned_layers and True in [ftl in n for ftl in self.finetuned_layers]:
                 p.requires_grad = True
             else: 
                 p.requires_grad = False
