eb33cb107403d84940c40e1b587714f8f7e97aa4
lodewijkvkeizerswaard <lodewijk.keizer@gmail.com>
2021-05-07 13:11:55 +0200

    Added optimizer to model save path

--- train_bert.py
+++ train_bert.py
@@ -10,7 +10,7 @@ from berttrainer import BERTTraniner
 from bertutil import get_args
 
 def train_bert(loader, conf):
-    trainer = pl.Trainer(default_root_dir=os.path.join(conf.path, conf.dataset, conf.name),
+    trainer = pl.Trainer(default_root_dir=os.path.join(conf.path, conf.dataset, conf.optimizer, conf.name),
                          checkpoint_callback = ModelCheckpoint(save_weights_only=True, mode="max", monitor="train_acc" if loader['val'] == None 
                                                                                                             else "val_acc"),
                          gpus=1 if "gpu" in str(conf.device) else 0,
