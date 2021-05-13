6e9bcb0b1c768307bc8460f104cbaa91df4c03eb
elfrink1 <auke.elfrink@gmail.com>
2021-05-13 16:24:16 +0200

    further reduced mem usage load_ng

--- multitask_data.py
+++ multitask_data.py
@@ -27,7 +27,7 @@ class MultitaskDataset():
             The description is empty for some samples, which is why it is not returned."""
         txt = [head + ' ' + desc for head, desc in zip(data["headline"], data["short_description"])]
         maxlength = [ex[:max_text_length] for ex in txt]
-        txt = tokenizer(maxlength, padding=True, return_tensors='pt')["input_ids"]
+        txt = tokenizer(maxlength, padding=True, return_tensors='pt', truncation=True, max_length=512)['input_ids']
         labels = torch.LongTensor(data["category_num"])
         return [{"txt" : h, "label" : l} for h, l in zip(txt, labels)]
 
@@ -47,7 +47,7 @@ class MultitaskDataset():
             The headlines contain '\\' characters in place of newlines. """
         maxlength = [ex[:max_text_length] for ex in data["text"]]
         noslash = [ex.replace("\\", " ") for ex in maxlength]
-        headlines = tokenizer(noslash, padding=True, return_tensors='pt')["input_ids"]
+        headlines = tokenizer(noslash, padding=True, return_tensors='pt', truncation=True, max_length=512)['input_ids']
         labels = torch.LongTensor(data["label"])
         return [{"txt" : h, "label" : l} for h, l in zip(headlines, labels)]
 
@@ -58,19 +58,25 @@ class MultitaskDataset():
             Splits in train (1490) and test (735)."""
         l2i = {'entertainment':0, 'business':1, 'politics':2, 'sport':3, 'tech':4}
         # first row are colum names: ['ArticleId' 'Text' 'Category']
-        train = [line.replace("\n", "").split(",") for line in open('Data/BBC/Train/BBC News Train.csv')][1:]
-        train = [[ex[1], l2i[ex[2]]] for ex in train]
+        with open('Data/BBC/Train/BBC News Train.csv') as train_file:
+            train = [line.replace("\n", "").split(",") for line in train_file][1:]
+            train = [[ex[1], l2i[ex[2]]] for ex in train]
 
         # first row are colum names: ['ArticleId' 'Text']
-        test_ex = [line.replace("\n", "").split(",") for line in open('Data/BBC/Test/BBC News Test.csv')][1:]
+        with open(r'Data/BBC/Test/BBC News Test.csv') as test_file:
+            test_ex = [line.replace("\n", "").split(",") for line in test_file][1:]
+
         # first row are colum names: ['ArticleId' 'Category']
-        test_labels = [line.replace("\n", "").split(",") for line in open('Data/BBC/Test/BBC News Sample Solution.csv')][1:]
+        with open(r'Data/BBC/Test/BBC News Sample Solution.csv') as test_labels_file:
+            test_labels = [line.replace("\n", "").split(",") for line in test_labels_file][1:]
+
         test = [[ex[1], l2i[label[1]]] for ex, label in zip(test_ex, test_labels) if ex[0] == label[0]]
 
         train = self.process_bbc(train, tokenizers[conf.tokenizer], conf.max_text_length)
         val = None
         test = self.process_bbc(test, tokenizers[conf.tokenizer], conf.max_text_length)
 
+        print(len(test), len(test_labels))
         assert len(test) == len(test_labels)
         "The BBC test datafiles were corrupted!"
 
@@ -80,7 +86,7 @@ class MultitaskDataset():
     def process_bbc(self, data, tokenizer, max_text_length=-1):
         """ Extracts the headlines and labels from the BBC news dataset. """
         maxlength = [b[0][:max_text_length] for b in data]
-        headlines = tokenizer(maxlength, padding=True, return_tensors='pt')["input_ids"]
+        headlines = tokenizer(maxlength, padding=True, return_tensors='pt', truncation=True, max_length=512)['input_ids']
 
         labels = torch.LongTensor([b[1] for b in data])
 
@@ -131,8 +137,7 @@ class MultitaskDataset():
         """ Extracts the headlines and labels from the 20 newsgroups dataset. """
 
         text = [b[0] for b in data]
-        text = tokenizer(text, padding=True, return_tensors='pt')["input_ids"]
-        text = [h if len(h) < 512 else h[:512] for h in text]
+        text = tokenizer.encode(text, padding=True, return_tensors='pt', truncation=True, max_length=512)
 
         labels = [b[1] for b in data]
         labels = torch.LongTensor(labels)
@@ -170,23 +175,23 @@ class MultitaskDataset():
         ''' Returns the data in a list of batches of size bs'''
         return [[data[0][i:i+bs], data[1][i:i+bs]] for i in range(0, len(data), bs)]
 
-# class Args():
-#     def __init__(self):
-#         self.path = "models/bert"
-#         self.optimizer = "Adam"
-#         self.lr = 0.001
-#         self.max_epochs = 100
-#         self.finetuned_layers = 0
-#         self.tokenizer = "BERT"
-#         self.batch_size = 64
-#         self.device = "gpu"
-#         self.seed = 20
-#         self.max_text_length = -1
-#
-# if __name__ == "__main__":
-#     conf = Args()
-#     multitask_data = MultitaskDataset(conf)
-#     print("datapoint hp:", multitask_data.train['hp'][0])
-#     print("datapoint ag:", multitask_data.train['ag'][0])
-#     print("datapoint bbc:", multitask_data.train['bbc'][0])
-#     print("datapoint ng:", multitask_data.train['ng'][0])
+class Args():
+    def __init__(self):
+        self.path = "models/bert"
+        self.optimizer = "Adam"
+        self.lr = 0.001
+        self.max_epochs = 100
+        self.finetuned_layers = 0
+        self.tokenizer = "BERT"
+        self.batch_size = 64
+        self.device = "gpu"
+        self.seed = 20
+        self.max_text_length = -1
+
+if __name__ == "__main__":
+    conf = Args()
+    multitask_data = MultitaskDataset(conf)
+    print("datapoint hp:", multitask_data.train['hp'][0], '\n')
+    print("datapoint ag:", multitask_data.train['ag'][0], '\n')
+    print("datapoint bbc:", multitask_data.train['bbc'][0], '\n')
+    print("datapoint ng:", multitask_data.train['ng'][0], '\n')
