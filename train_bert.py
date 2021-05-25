a311f94788bb5cb79277cfdc9be899f7bf6f66c0
lodewijkvkeizerswaard <lodewijk.keizer@gmail.com>
2021-05-25 17:12:36 +0200

    Added testing functionality

--- train_bert.py
+++ train_bert.py
@@ -10,11 +10,13 @@ from berttrainer import BERTTraniner
 from bertutil import get_args
 
 def train_bert(loader, conf):
-    """ Train a BERT baseline model (BertClassifier) on the given dataset. The model that performs best on the validation set is saved.
-        If no validation set is given, the model with the best performance on the training set is saved. """
+    """ Train or test a BERT baseline model (BertClassifier) on the given dataset. 
+        During training, the model that performs best on the validation set is saved. If no validation set is given, 
+        the model with the best performance on the training set is saved. 
+        Testing is done on the given datasets test split. """
+
     trainer = pl.Trainer(default_root_dir=os.path.join(conf.path, conf.dataset, conf.optimizer, conf.name),
-                         checkpoint_callback = ModelCheckpoint(save_weights_only=True, mode="max", monitor="train_acc" if loader['val'] == None 
-                                                                                                            else "val_acc"),
+                         callbacks = ModelCheckpoint(save_weights_only=True, mode="max", monitor="val_acc"),
                          gpus=1 if "gpu" in str(conf.device) else 0,
                          max_epochs=conf.max_epochs,                                            
                          progress_bar_refresh_rate= 1 if conf.progress_bar else 0) 
@@ -38,6 +40,19 @@ def train_bert(loader, conf):
 
     return model, test_result
 
+def test_bert(loader, conf):
+    trainer = pl.Trainer(default_root_dir=os.path.join(conf.path, conf.dataset, conf.optimizer, conf.name),
+                         checkpoint_callback = ModelCheckpoint(save_weights_only=True, mode="max", monitor="train_acc" if loader['val'] == None 
+                                                                                                            else "val_acc"),
+                         gpus=1 if "gpu" in str(conf.device) else 0,
+                         max_epochs=conf.max_epochs,                                            
+                         progress_bar_refresh_rate= 1 if conf.progress_bar else 0) 
+    
+    model = BERTTraniner.load_from_checkpoint(conf.test_model)
+    test_result = trainer.test(model, loader['test'])
+
+    return model, test_result
+
         
 if __name__ == "__main__":
     conf = get_args()
@@ -51,6 +66,9 @@ if __name__ == "__main__":
         'val'   : data.DataLoader(dataset.val, batch_size=conf.batch_size, shuffle=False, drop_last=False, num_workers=4) if dataset.val != None else None,
         'test'  : data.DataLoader(dataset.test, batch_size=conf.batch_size, shuffle=False, drop_last=False, num_workers=4) if dataset.test != None else None
     }
+    if conf.test_model == "": # We are training
+        model, results = train_bert(loader, conf)
+    else: # We are testing
+        model, results = test_bert(loader, conf)
 
-    model, results = train_bert(loader, conf)
     print("Results:", results)
